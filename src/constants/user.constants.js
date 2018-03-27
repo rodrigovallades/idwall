460120aec93bc16a32f3098583412ac01bc6f090
rodrigovallades <rodrigo.vallades@gmail.com>
2018-03-27 18:40:14 -0300

    /whoops

--- src/constants/user.constants.js
+++ src/constants/user.constants.js
@@ -1,5 +1,5 @@
 export default  {
-  LOGIN_API: 'https://iddog-api.now.sh/signin',
+  LOGIN_API: 'https://iddog-api.now.sh/signup',
   LOGIN_REQUEST: 'USER_LOGIN_REQUEST',
   LOGIN_SUCCESS: 'USER_LOGIN_SUCCESS',
   LOGIN_FAILURE: 'USER_LOGIN_FAILURE'
