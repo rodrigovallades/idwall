fc25da5aca38ab84df23e23f865ca35b919091a4
rodrigovallades <rodrigo.vallades@gmail.com>
2018-03-28 11:24:54 -0300

    Improve route handling if route not found

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import Signin from '../signin'
 import Feed from '../feed'
@@ -10,13 +10,16 @@ const App = () => (
   <div className='wrapper'>
     <header>THE IDDOG</header>
     <main>
-      <Route exact path="/" component={Signin} />
-      <Route exact path="/feed" component={Feed} />
-      <Route exact path="/husky" render={(props) => ( <Feed category='husky'/> )} />
-      <Route exact path="/labrador" render={(props) => ( <Feed category='labrador'/> )} />
-      <Route exact path="/hound" render={(props) => ( <Feed category='hound'/> )} />
-      <Route exact path="/pug" render={(props) => ( <Feed category='pug'/> )} />
-      <Route component={Signin}/>
+      <Switch>
+        <Route exact path="/" component={Signin} />
+        <Route exact path="/feed" component={Feed} />
+        <Route exact path="/husky" render={(props) => ( <Feed category='husky'/> )} />
+        <Route exact path="/labrador" render={(props) => ( <Feed category='labrador'/> )} />
+        <Route exact path="/hound" render={(props) => ( <Feed category='hound'/> )} />
+        <Route exact path="/pug" render={(props) => ( <Feed category='pug'/> )} />
+        <Redirect from="*" to="/" />
+        <Route component={Signin}/>
+      </Switch>
     </main>
   </div>
 )
